6811c8539eca284c14296085a23c4087e8f8d97d
almeow2411 <wanglemei365@gmail.com>
2026-01-21 18:00:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.editable-image').forEach(img => {
-    img.onclick = () => {
-        const url = prompt('Nhập link ảnh mới:');
-        if(url) img.src = url;
-    };
-});
-function showScreen(id) { alert('Bạn vừa bấm vào app: ' + id); }
